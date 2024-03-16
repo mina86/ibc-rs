b20f8b1fba4df337448a8896215963a50e7cd8b8
dhruvja <dhruv@iamsizzling.com>
2024-03-15 21:35:41 -0300

    fix

--- ibc-clients/ics07-tendermint/types/src/client_state/solana_verifier.rs
+++ ibc-clients/ics07-tendermint/types/src/client_state/solana_verifier.rs
@@ -28,15 +28,15 @@ impl tendermint::crypto::signature::Verifier for SigVerifier {
         } else {
             return Err(Error::UnsupportedKeyType);
         };
-        let pubkey = <&sigverify::PubKey>::try_from(pubkey.as_bytes())
+        let pubkey = <&solana_ed25519::PubKey>::try_from(pubkey.as_bytes())
             .map_err(|_| Error::MalformedPublicKey)?;
-        let sig = <&sigverify::Signature>::try_from(signature.as_bytes())
+        let sig = <&solana_ed25519::Signature>::try_from(signature.as_bytes())
             .map_err(|_| Error::MalformedSignature)?;
 
         // SAFETY: The function is always safe to run and if it returns non-null
         // than it returns a pointer to an object with static lifetime thus it’s
         // always sound to dereference it.
-        let verifier: Option<&sigverify::Verifier> = unsafe {
+        let verifier: Option<&solana_ed25519::Verifier> = unsafe {
             let ptr = get_global_ed25519_verifier() as *mut ();
             core::ptr::NonNull::new(ptr).map(|ptr| ptr.cast().as_ref())
         };
