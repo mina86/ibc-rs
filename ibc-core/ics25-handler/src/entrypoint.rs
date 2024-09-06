2c56f25202762c43b7e15f26ca4f4aa19c1994f8
dhruvja <dhruv@iamsizzling.com>
2024-09-06 23:47:15 +0530

    use match instead of nested ifs

--- ibc-core/ics25-handler/src/entrypoint.rs
+++ ibc-core/ics25-handler/src/entrypoint.rs
@@ -27,25 +27,16 @@ pub fn dispatch(
     router: &mut impl Router,
     msg: MsgEnvelope,
 ) -> Result<(), ContextError> {
-    if matches!(msg, MsgEnvelope::Client(ClientMsg::UpdateClient(_))) {
-        let header = match msg {
-            MsgEnvelope::Client(ref msg) => match msg {
-                ClientMsg::UpdateClient(msg) => msg,
-                _ => panic!("Invalid message type"),
-            },
-            _ => panic!("Invalid message type"),
-        };
-        if header.client_id.as_str().contains("tendermint") {
-            let header =
-                ibc_client_tendermint_types::Header::try_from(header.clone().client_message)
-                    .unwrap();
-            validate(ctx, router, msg.clone(), Some(header.clone()))?;
-            execute(ctx, router, msg, Some(header))?;
-            return Ok(())
+    let header = match msg {
+        MsgEnvelope::Client(ClientMsg::UpdateClient(ref msg))
+            if msg.client_id.as_str().contains("tendermint") =>
+        {
+            ibc_client_tendermint_types::Header::try_from(msg.client_message.clone()).ok()
         }
-    }
-    validate(ctx, router, msg.clone(), None)?;
-    execute(ctx, router, msg, None)?;
+        _ => None,
+    };
+    validate(ctx, router, msg.clone(), header.clone())?;
+    execute(ctx, router, msg, header)?;
 
     Ok(())
 }
